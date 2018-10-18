c26637421a49009837b0b45fb79fe713647fb5dc
Сергей <sergey.kononov@ema.ru>
2018-10-18 22:47:41 +0700

    Initial Commit

---
+++ Yo.ListOfBestFilms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yo.ListOfBestFilms
+//
+//  Created by Сергей on 18/10/2018.
+//  Copyright © 2018 Sergei Kononov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
